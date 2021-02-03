b5f20440d916db6218d72c7b769d3f6819694567
Lionshide <31610964+Lionshide@users.noreply.github.com>
2021-02-03 12:43:08 +0000

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' Ares';
+const name = ' Sera';
 
 // Here you can change your greetings
 const gree1 = 'Go to Sleep!  ';
